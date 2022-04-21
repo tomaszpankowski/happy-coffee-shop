6a8f8bb707cee6321c07ef59c991f9f994f8d2eb
tomaszpankowski <--get-all>
2022-04-21 13:31:06 +0200

    project's initial state

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+import { ErrorComponent } from './error/error.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: ComingsoonComponent
+  },
+  {
+    path: "privacy",
+    component: PrivacyComponent
+  },
+  {
+    path:"**",
+    component: ErrorComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    PrivacyComponent,
+    ComingsoonComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
